a5d62ba4550a1b626794ad30832b66038350e762
Takashi <gon27.tk08@gmail.com>
2020-07-19 15:12:55 +0900

    GameLibヘッダーを作成

--- Game2DProject/GameLib.h
+++ Game2DProject/GameLib.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef GAMELIB_H
+#define GAMELIB_H
+
+#include "App.h"
+
+#include "Console.h"
+
+#include "Window.h"
+
+#include "Graphics.h"
+
+#include "Render.h"
+
+#include "Input.h"
+
+#endif // !GAMELIB_H
