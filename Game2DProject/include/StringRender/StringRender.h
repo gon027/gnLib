ec4994257e06d934d227fd0581ffd9b68cdd789d
Takashi <gon27.tk08@gmail.com>
2020-10-15 06:34:45 +0900

    [リファクタリング] StringRenderの要らない関数を削除

--- Game2DProject/include/StringRender/StringRender.h
+++ Game2DProject/include/StringRender/StringRender.h
@@ -8,26 +8,18 @@
 #include "../Color/Color.h"
 
 namespace gnLib {
-	class Graphics;
 
 	// •¶š—ñ‚ğ‰æ–Êã‚É•`‰æ‚·‚éƒNƒ‰ƒX
 	class StringRender {
 		using string = std::string;
 
 	public:
-		StringRender();
-		~StringRender();
+		StringRender()  = default;
+		~StringRender() = default;
 
-		//void setFont(string _fontName);
-		//void setFontSize(int _fontSize);
-		bool init();
-		void drawText(const string& _str, int _x, int _y, Color _color);
-
-	private:
-		int width = 10;
-		int height = 20;
-		int fontSize;
+		void drawText(const string& _str, int _x, int _y, Color _color = Color::Black);
 	};
+
 }
 
 #endif // !STRINGRENDER_H
--- Game2DProject/src/StringRender/StringRender.cpp
+++ Game2DProject/src/StringRender/StringRender.cpp
@@ -1,51 +1,17 @@
 #include "../../include/StringRender/StringRender.h"
 #include "../../include/Render/Render.h"
 #include "../../include/Graphics/Graphics.h"
-#include "../../include/Common/Macro.h"
 #include "../../include/GameCore/GameCore.h"
+#include "../../include/Window/Window.h"
 
 namespace gnLib {
-	StringRender::StringRender()
-	{
-	}
-
-	StringRender::~StringRender()
-	{
-		//RELEASE(font);
-	}
-
-	bool StringRender::init()
-	{
-		/*
-		HRESULT ret;
-
-		ret = D3DXCreateFont(
-			GCGraphics,
-			height,
-			width,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			0,
-			"‚l‚r ƒSƒVƒbƒN",
-			&font
-		);
-
-		if (FAILED(ret)) {
-			return false;
-		}
-		*/
-
-		return true;
-	}
 
 	void StringRender::drawText(const string& _str, int _x, int _y, Color _color)
 	{
 		// left, top, right, bottom
-		RECT rc{ _x, _y, 480, 600 };
+		RECT rc{ 
+			_x, _y, WindowInfo::Width, WindowInfo::Height
+		};
 
 		GCStrDevice->DrawText(
 			NULL,
